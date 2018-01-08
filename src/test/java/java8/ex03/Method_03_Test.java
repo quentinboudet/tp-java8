6f0ca11ea93551e0ec39a3fcccfa141327506dfb
quentinboudet <quentin.boudet@outlook.fr>
2018-01-08 14:08:46 +0100

    TP 03

--- src/test/java/java8/ex03/Method_03_Test.java
+++ src/test/java/java8/ex03/Method_03_Test.java
@@ -16,6 +16,11 @@ public class Method_03_Test {
     interface IDao {
         List<Person> findAll();
 
+        public static IDao getDefaultInstance() {
+        	Method_03_Test M03T = new Method_03_Test();
+        	DaoA daoA = M03T.new DaoA();
+        	return daoA;
+        }
         // TODO créer une méthode statique IDao getDefaultInstance()
         // TODO cette méthode retourne une instance de la classe DaoA
     }
@@ -35,7 +40,7 @@ public class Method_03_Test {
     @Test
     public void test_getDefaultInstance() throws Exception {
         // TODO invoquer la méthode getDefaultInstance() pour que le test soit passant
-        IDao result = null;
+        IDao result = IDao.getDefaultInstance();
 
         assert result.findAll().size() == 20;
     }
